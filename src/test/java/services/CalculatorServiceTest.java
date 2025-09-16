169a88d5e8f98df395c7edbd09370442eefd457e
BetaCom16 <betacom16@outlook.de>
2025-09-16 14:34:05 +0200

    Unittests und CI Pipeline

---
+++ src/test/java/services/CalculatorServiceTest.java
@@ -0,0 +1,20 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+class CalculatorServiceTest {
+
+    private final CalculatorService calculator = new CalculatorService();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+}
