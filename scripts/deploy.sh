5f04a18246416eff6459742238bdb9f3fbe7faed
BetaCom16 <betacom16@outlook.de>
2025-09-17 16:30:14 +0200

    Deploy user auf Ubuntu angepasst

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@
 sudo apt-get update -y
 sudo apt-get install -y docker.io docker-compose python3 python3-pip
 
-# 2. Install AWS CLI using pip with --break-system-packages
-sudo pip3 install awscli --break-system-packages
+# 2. Add the ubuntu user to the docker group
+sudo usermod -aG docker ubuntu
 
 # 3. Start Docker services
 sudo systemctl start docker
@@ -15,10 +15,11 @@ sudo systemctl enable docker
 cd /home/ubuntu/my-app
 
 # 5. Login to ECR
-sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-north-1) 443526375408.dkr.ecr.eu-north-1.amazonaws.com/devops-app
+sudo pip3 install awscli --break-system-packages
+sudo docker login -u AWS -p $(aws ecr get-login-password --eu-north-1) 443526375408.dkr.ecr.eu-north-1.amazonaws.com/devops-app
 
-# 6. Stop and remove the old container to avoid migration errors
-sudo docker-compose down
+# 6. Stop and remove the old container
+docker-compose down
 
 # 7. Pull the latest image and start the application in detached mode
-sudo docker-compose up -d --pull always
+docker-compose up -d --pull always
