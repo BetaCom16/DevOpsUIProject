ab7f169ca50a27ed59058874caaeb39b6d37192b
BetaCom16 <betacom16@outlook.de>
2025-09-17 15:17:45 +0200

    ECR angepasst

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y docker.io docker-compose
+sudo apt-get install -y docker.io docker-compose awscli
 sudo systemctl start docker
 sudo systemctl enable docker
--- scripts/pull_image.sh
+++ scripts/pull_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/my-app
+sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-north-1) 443526375408.dkr.ecr.eu-north-1.amazonaws.com/devops-app
 sudo docker-compose pull
