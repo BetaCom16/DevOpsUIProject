6ac41dff41594f80c3327e326af7ed5e5040e87d
BetaCom16 <betacom16@outlook.de>
2025-09-17 15:36:02 +0200

    PIP Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# 1. Update system and install dependencies (Docker, AWS CLI)
+# 1. Update system and install dependencies (Docker)
 sudo apt-get update -y
-sudo apt-get install -y docker.io docker-compose awscli
+sudo apt-get install -y docker.io docker-compose python3 python3-pip
+
+# 2. Install AWS CLI using pip
+sudo pip3 install awscli
+
+# 3. Start Docker services
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# 2. Navigate to the app directory
+# 4. Navigate to the app directory
 cd /home/ubuntu/my-app
 
-# 3. Login to ECR
-sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-north-1) 443526375408.dkr.ecr.eu-north-1.amazonaws.com/devops-app
+# 5. Login to ECR
+sudo docker login -u AWS -p $(aws ecr get-login-password --region <DEINE_REGION>) <DEINE_ECR_REPOSITORY_URL>
 
-# 4. Pull the latest image and start the application
+# 6. Pull the latest image and start the application
 sudo docker-compose pull
 sudo docker-compose up -d
