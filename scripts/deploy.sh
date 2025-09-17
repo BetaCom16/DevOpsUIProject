da046594358fba59096df6b4aaf1aee332887892
BetaCom16 <betacom16@outlook.de>
2025-09-17 15:43:54 +0200

    PIP Ignore untrusted Packages Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@
 sudo apt-get update -y
 sudo apt-get install -y docker.io docker-compose python3 python3-pip
 
-# 2. Install AWS CLI using pip
-sudo pip3 install awscli
+# 2. Install AWS CLI using pip with --break-system-packages
+sudo pip3 install awscli --break-system-packages
 
 # 3. Start Docker services
 sudo systemctl start docker
@@ -15,7 +15,7 @@ sudo systemctl enable docker
 cd /home/ubuntu/my-app
 
 # 5. Login to ECR
-sudo docker login -u AWS -p $(aws ecr get-login-password --region eu-north-1) 443526375408.dkr.ecr.eu-north-1.amazonaws.com/devops-app
+sudo docker login -u AWS -p $(aws ecr get-login-password --region <DEINE_REGION>) <DEINE_ECR_REPOSITORY_URL>
 
 # 6. Pull the latest image and start the application
 sudo docker-compose pull
